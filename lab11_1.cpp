1a8e51a953cb872494f41d32dec2909b5e9989c1
Jsukrit <sukritdn1@gmail.com>
2024-12-23 22:37:14 +0700

    finished

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,9 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Hello";
+}
